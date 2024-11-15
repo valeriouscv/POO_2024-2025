f4bd24735ddd13e34fab72426f06c68209a01495
Valério Santos <valerio.santos@us.edu.cv>
2024-11-15 15:44:10 -0100

    Data
    atualizado

--- aula07/Data.java
+++ aula07/Data.java
@@ -4,6 +4,52 @@ public class Data{
     private int mes;
     private int dia;
 
-    
+    //construtor
+    public Data(int dia, int mes, int ano){
+        setAno(ano);
+        setMes(mes);
+        setDia(dia);
+    }
+    //getters 
+    public int getAno(){
+        return this.ano;
+    }
+    public int getMes(){
+        return this.mes;
+    }
+    public int getDia(){
+        return this.dia;
+    }
 
+    //setters
+    public void setAno(int ano){
+        if(ano <0){
+            System.out.println("Ano invalido. Vai ser colocado a um");
+            this.ano =1;
+            return;
+        }
+        this.ano = ano;
+    }
+    public void setMes(int mes){
+        if(mes<1 || mes >12){
+            System.out.println("Mes invalido. Vai ser colocado a 1");
+            this.mes = 1;
+            return;
+        }
+        this.mes = mes;
+    }
+    public void setDia(int dia){
+        if(dia<1 || dia >31){
+            System.out.println("Dia invalido. Vai ser colocado a 1");
+            this.dia = 1;
+            return;
+        }
+        this.dia=dia;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return dia +"/"+mes+"/"+ano;
+    }
 }
--- aula07/DataTeste.java
+++ aula07/DataTeste.java
@@ -1,5 +1,7 @@
 public class DataTeste {
     public static void main(String[] args) {
-        Data d = new Data();
+        Data d = new Data(14,7,-1);
+        d.setAno(2005);
+        System.out.println(d);
     }
 }
