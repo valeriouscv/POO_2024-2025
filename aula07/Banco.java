55fc2eb05958b0dfc31fcbb6916eb92e5ddaca31
Valério Santos <valerio.santos@us.edu.cv>
2024-11-19 17:09:54 -0100

    Banco e Conta
    atualização

--- aula07/Banco.java
+++ aula07/Banco.java
@@ -1,7 +1,16 @@
 public class Banco {
     //metodos
     public static boolean transferencia(Conta origem, Conta destino, double quantia){
-        //para implementar
+        if(origem.podeLevantar(quantia)){
+            //na origem
+            int novoSaldo = origem.getSaldo() - quantia;
+            origem.setSaldo(novoSaldo);
+            
+            //no destino
+            novoSaldo = destino.getSaldo() + quantia;
+            destino.setSaldo(novoSaldo);
+            return true;
+        }
         return false;
     }
     public static void main(String[] args) {
--- aula07/Conta.java
+++ aula07/Conta.java
@@ -6,10 +6,17 @@ public class Conta {
 
   //metodos
   public void levantamento(double quantia){
-    //implementar o código
+    if(podeLevantar(quantia)){
+      System.out.println("Saldo Anterior   : "+this.saldo);
+      System.out.println("Valor a levantar : "+quantia);
+      this.saldo = this.saldo - quantia; //fez-se o levantamento. atualizar o saldo
+      System.out.println("Saldo Atual      : "+this.saldo);
+    }else{
+      System.out.println("Nao pode efetuar esta operacao!");
+    }
   }
   public boolean podeLevantar(double quantia){
-    //implementar o código
+    if(quantia <= saldo) return true;
     return false;
   }
 
