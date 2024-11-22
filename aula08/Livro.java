737aa46a2007f601dcfc6a8e14bc416002b7e5af
Valério Santos <valerio.santos@us.edu.cv>
2024-11-22 16:38:34 -0100

    Livro
    Versão final

--- aula08/Livro.java
+++ aula08/Livro.java
@@ -1,12 +1,16 @@
 public class Livro {
     //atributos
-    private String autor;
-    private String titulo;
+    private String  autor;
+    private String  titulo;
+    private int     paginas;
+    private String  numReferencia;
 
     //construtor
-    public Livro(String autor, String titulo){
+    public Livro(String autor, String titulo, int paginas){
         this.titulo=titulo;
         this.autor=autor;
+        this.paginas=paginas;
+        this.numReferencia="";
     }
 
     //modificadores de acesso
@@ -15,5 +19,44 @@ public class Livro {
     }
     public String getTitulo(){
         return this.titulo;
+    }
+    public int getPaginas(){
+        return this.paginas;
+    }
+    public String getNumReferencia(){
+        return this.numReferencia;
+    }
+
+    //setters
+    public void setNumReferencia(String numReferencia){
+        if(numReferencia.length() > 3){
+            this.numReferencia = numReferencia;
+        }else{
+            System.out.println("Erro. Tamanho do numReferencia inferior ao permitido. Deve ter no mínimo 3 carateres!");
+        }
+      
+    }
+
+    //outros métodos
+    public void imprimeAutor(){
+        System.out.println("Autor: "+this.autor);
+    }
+    public void imprimeTitulo(){
+        System.out.println("Título: "+this.titulo);
+    }
+    public void imprimirDetalhes(){
+        System.out.println("\n+--------------------------------------+");
+        System.out.println("|              --> Livro <---          |");
+        System.out.println("+--------------------------------------+");
+        System.out.println("  Titulo               : "+this.titulo);
+        System.out.println("  Autor                : "+this.autor);
+        System.out.println("  Número de páginas    : "+this.paginas);
+        System.out.println("  Numéro de Referencia : "+(this.numReferencia.length()==0 ? "ZZZ":this.numReferencia));
+        //sem usar o operador ternario
+        /*if(this.numReferencia.length() == 0){
+            System.out.println("  Numéro de Referencia : ZZZ");
+        }else{
+            System.out.println("  Numéro de Referencia : "+this.numReferencia);
+        }*/
     }
 }
--- aula08/LivroTeste.java
+++ aula08/LivroTeste.java
@@ -1,10 +1,15 @@
 public class LivroTeste {
     public static void main(String[] args) {
-        Livro a = new Livro("Franz Kafka", "A Metamorfose");
-        Livro b = new Livro("Baltazar Lopes", "Chiquinho");
-        Livro c = new Livro("Robert Kiyosaki", "Pai Rico, Pai Pobre");
+        Livro a = new Livro("Franz Kafka", "A Metamorfose",112);
+        Livro b = new Livro("Baltazar Lopes", "Chiquinho",212);
+        Livro c = new Livro("Robert Kiyosaki", "Pai Rico, Pai Pobre",336);
 
-        System.out.println("Autor  : "+b.getAutor());
-        System.out.println("Título : "+b.getTitulo());
+        //System.out.println("Autor  : "+b.getAutor());
+        //System.out.println("Título : "+b.getTitulo());
+        a.setNumReferencia("8571646856");
+        b.setNumReferencia("58");
+        a.imprimirDetalhes();
+        b.imprimirDetalhes();
+
     }
 }
