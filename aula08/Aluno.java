dfbeb2238e0662c0db19fbf6b83dcf536b7d7798
Valério Santos <valerio.santos@us.edu.cv>
2024-11-22 16:47:14 -0100

    Aluno e Turma
    Construção das classes

---
+++ aula08/Aluno.java
@@ -0,0 +1,6 @@
+public class Aluno {
+    //atributos
+    private String nome;
+    private int    idade;
+    private String telefone;
+}
---
+++ aula08/Turma.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Turma {
+    private ArrayList<Aluno> lista;
+
+    public Turma(){
+        lista = new ArrayList<>();
+    }
+}
