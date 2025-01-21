db186630e39fbf9770725d898494ce8814a2d5f4
Valério Santos <130845898+valeriouscv@users.noreply.github.com>
2025-01-21 15:21:32 -0100

    Classes Abstratas e Interfaces

---
+++ aula11/code/TestarAnimais.java
@@ -0,0 +1,13 @@
+public class TestarAnimais {
+    public static void main(String[] args) {
+        Crocodile viziz = new Crocodile();
+        viziz.come();
+        viziz.dorme();
+
+        Gato miau = new Gato();
+        miau.come();
+        miau.dorme();
+        miau.reproduz();
+        miau.tem();
+    }
+}
